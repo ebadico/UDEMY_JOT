1fc8267177f4e19e9ce9fbcfef53cc78e82e0cd0
Stoyan Kalinov <bloodyrax@gmail.com>
2019-11-12 18:04:44 +0200

    add simple validation and tests for contact store

--- app/Contact.php
+++ app/Contact.php
@@ -4,6 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Contact
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Contact whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Contact extends Model
 {
     protected $guarded = [];
--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -20,8 +20,47 @@ class ContactsTest extends TestCase
 
         $this->post('api/contacts', [
             'name' => 'Test name',
+            'email' => 'email@example.com',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String',
         ]);
 
-        $this->assertCount(1, Contact::all());
+        $contact = Contact::first();
+
+        $this->assertEquals('Test name', $contact->name);
+        $this->assertEquals('email@example.com', $contact->email);
+        $this->assertEquals('05/14/1988', $contact->birthday);
+        $this->assertEquals('ABC String', $contact->company);
+
+    }
+
+    /**
+    * @test
+    */
+    public function a_contact_requires_a_name()
+    {
+        $response = $this->post('api/contacts', [
+            'email' => 'email@example.com',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String',
+        ]);
+
+        $response->assertSessionHasErrors('name');
+        $this->assertCount(0, Contact::all());
+    }
+
+    /**
+    * @test
+    */
+    public function a_contact_requires_an_email()
+    {
+        $response = $this->post('api/contacts', [
+            'name' => 'Test name',
+            'birthday' => '05/14/1988',
+            'company' => 'ABC String',
+        ]);
+
+        $response->assertSessionHasErrors('email');
+        $this->assertCount(0, Contact::all());
     }
 }
