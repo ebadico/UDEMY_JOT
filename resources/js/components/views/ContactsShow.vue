51b8e87a54b2957adefd8d4e811faea4a0004c88
STOYO <bloodyrax@gmail.com>
2019-11-23 16:29:40 +0200

    Add styling for 'ContactShow' component

--- resources/js/components/views/ContactsShow.vue
+++ resources/js/components/views/ContactsShow.vue
@@ -1,5 +1,29 @@
 <template>
-	
+	<div>
+		<div class="flex justify-between">
+			<div class="text-blue-400">
+				< Back
+			</div>
+			<div>
+				<router-link :to="'/contact/' + contact.id + '/edit'"
+				             class="px-4 py-2 rounded text-sm font-bold text-green-500 border border-green-500 mr-2">Edit</router-link>
+				<a href="#" class="px-4 py-2 rounded border border-red-500 text-sm font-bold text-red-500">Delete</a>
+			</div>
+		</div>
+
+		<div class="flex items-center pt-6">
+			<div>VG</div>
+
+			<p v-text="contact.name" class="pl-5 text-xl"></p>
+		</div>
+
+		<p class="pt-6 text-gray-600 font-bold uppercase  text-sm">Contact</p>
+		<p class="pt-2 text-blue-400" v-text="contact.email"></p>
+		<p class="pt-6 text-gray-600 font-bold uppercase  text-sm">Company</p>
+		<p class="pt-2 text-blue-400" v-text="contact.company"></p>
+		<p class="pt-6 text-gray-600 font-bold uppercase  text-sm">Birthday</p>
+		<p class="pt-2 text-blue-400" v-text="contact.birthday"></p>
+	</div>
 </template>
 
 <script>
